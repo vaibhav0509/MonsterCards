cbdfec91615aba60ad2f847f1a8b8f1e3efdbb75
Vaibhav <vaibhav.mishra0509@gmail.com>
2020-04-21 11:48:55 +0530

    build

---
+++ build/precache-manifest.3dcb11f341c8785f6a7b717238343520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1153a886abdead80a26623d17dfc989b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995ff1fba986f1531f47",
+    "url": "/static/css/main.6902ef38.chunk.css"
+  },
+  {
+    "revision": "f32f02700ffeaf5a2b14",
+    "url": "/static/js/2.bc3e73c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc3e73c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995ff1fba986f1531f47",
+    "url": "/static/js/main.e0075ee8.chunk.js"
+  },
+  {
+    "revision": "e2cf8b8cff80fa455d09",
+    "url": "/static/js/runtime-main.0f71ade6.js"
+  }
+]);
